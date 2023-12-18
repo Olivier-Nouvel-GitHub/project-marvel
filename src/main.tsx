6e8c906d24733029167dbbdde6b4b5604428c030
Olivier <olivier.nvl@gmail.com>
2023-12-18 14:05:03 +0100

    - react router end of config

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root />,
   },
 ]);
 
