1cba6d2125cb40b72cf01712687613e7ee9562dc
Olivier <olivier.nvl@gmail.com>
2024-02-29 12:56:20 +0100

    fix: unnused import removed

--- src/config/firebase.config.tsx
+++ src/config/firebase.config.tsx
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAGT7C3o6b-VV9VJ4pxWlp_aYsLAEmmV2Q",
